b6c319ce0564c33a9cb50847c421a9c14d71be69
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-07-21 17:03:37 +0200

    updated search function

--- search.js
+++ search.js
@@ -1,28 +1,26 @@
-const elasticsearch = require('elasticsearch');
+const elasticsearch = require('elasticsearch')
+
+const client = new elasticsearch.Client({
+  host: 'localhost:9200',
+})
 
 async function search(query) {
-  const client = new elasticsearch.Client({
-    host: 'localhost:9200',
-    log: 'trace',
-    apiVersion: '7.2', // use the same version of your Elasticsearch instance
-  });
-  // appeler elasticsearch pour effectuer la recherche
-  const result = await client.search({
-    index: 'books',
-    body: {
-      query: {
-        multi_match: {
-          query,
-          fields: [
-            'title^3',
-            'authors.name^2',
-            'subjects^1'
-          ]
-        }
-      }
-    }
-  })
-  return result.hits.hits;
+	const results = await client.search({
+		index: "books",
+		body: {
+			query: {
+				multi_match: {
+					query,
+					fields: [
+						"title^3",
+						"authors.name^2",
+						"subjects^1",
+					],
+				}
+			}
+		}
+	})
+	return results.hits
 }
 
 module.exports = search
