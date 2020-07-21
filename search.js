e3284af8054178b048aaba4200ceb1c01fc98d72
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-07-21 12:44:09 +0200

    added call request of elasticsearch

--- search.js
+++ search.js
@@ -1,13 +1,18 @@
+const elasticsearch = require('elasticsearch');
+
 async function search(query) {
+  const client = new elasticsearch.Client({
+    host: 'localhost:9200',
+    log: 'trace',
+    apiVersion: '7.2', // use the same version of your Elasticsearch instance
+  });
   // appeler elasticsearch pour effectuer la recherche
-  return [
-    {
-      name: "livre 1",
-    },
-    {
-      name: "livre 2",
-    },
-  ]
+  const result = await client.search({
+    index: 'books',
+    body: {}
+  })
+  console.log(result.hits.hits);
+  return result.hits.hits;
 }
 
 module.exports = search
