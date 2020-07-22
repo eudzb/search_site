b5f9a24f7b23af996c530f609f90f82d29d0ddaa
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-07-22 10:18:52 +0200

    added highlight on title

--- search.js
+++ search.js
@@ -8,15 +8,22 @@ async function search(query) {
   const results = await client.search({
 	index: "books",
 	body: {
-	query: {
-		multi_match: {
-		query,
-		fields: [
-			"title^3",
-			"authors.name^2",
-			"subjects^1",
-		],
-		fuzziness: "AUTO"
+	  query: {
+	    multi_match: {
+	      query,
+	      fields: [
+	  	  "title^3",
+	  	  "authors.name^2",
+	  	  "subjects^1",
+	      ],
+	    fuzziness: "AUTO"
+	    }
+	  },
+	  highlight : {
+		pre_tags : ["<b><i>"],
+        post_tags : ["</i></b>"],
+		fields : {
+		  title : {}
 		}
 	  }
 	}
