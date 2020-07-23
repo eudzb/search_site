3cbac6e9d7529a665a980fb6cb6aee0e6d1e3b0f
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-07-23 11:34:43 +0200

    added combined.log file that log request

--- app.js
+++ app.js
@@ -6,6 +6,25 @@ const PORT = process.env.PORT || 8080
 
 app.set("view engine", "ejs")
 
+const winston = require('winston');
+const expressWinston = require('express-winston');
+
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'combined.log', level: 'info' })
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json()
+  ),
+  meta: true,
+  msg: "HTTP {{req.method}} {{req.url}}",
+  expressFormat: true,
+  colorize: false,
+  ignoreRoute: function (req, res) { return false; }
+}));
+
 app.get("/", async (req, res) => {
   const aggs = await filterAuthor();
   res.render("homepage", { aggs })
